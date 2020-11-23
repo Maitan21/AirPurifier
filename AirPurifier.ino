cd2f997abd2f614e51059fe59e45149c6166e7c3
Maitan21 <oklfg2002@gmail.com>
2020-11-23 19:52:50 +0900

    Init AirPurifier

---
+++ AirPurifier.ino
@@ -0,0 +1,185 @@
+/* ***************************************************************************** */
+/* ************************** Header - LCD  ************************************ */
+/* ***************************************************************************** */
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_WIDTH    128
+#define LOGO_HEIGHT   64
+
+#include "bit_cat.h"
+
+/* ***************************************************************************** */
+/* ************************** Header - Dust Sensor  **************************** */
+/* ***************************************************************************** */
+int Vo = A0;
+int V_LED = 2;
+
+float Vo_value = 0;
+float Voltage = 0;
+float dustDensity = 0;
+
+/* ***************************************************************************** */
+/* ************************** Header - Dust Sensor  **************************** */
+/* ***************************************************************************** */
+int FAN = A1;
+
+void setup() {
+
+/* ***************************************************************************** */
+/* ************************** Setup - Dust Sensor  ***************************** */
+/* ***************************************************************************** */
+  Serial.begin(9600);
+
+  pinMode(V_LED, OUTPUT);
+  pinMode(Vo, INPUT);
+
+  delay(1000);
+  Serial.println();
+  Serial.println("**************************************");
+  Serial.println("***** Hello **************************");
+  Serial.println("***** Welcome to ROBOFRIEN world *****");
+  Serial.println("***** Thank you **********************");
+  Serial.println("**************************************");
+
+/* ***************************************************************************** */
+/* ************************** Setup - LCD  ************************************* */
+/* ***************************************************************************** */
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(WHITE);
+
+/* ********************** Image Slide ************************ */  
+  display.clearDisplay();
+  display.drawBitmap( 0, 0, cat1, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(500);
+
+  display.clearDisplay();
+  display.drawBitmap( 0, 0, cat2, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(300);
+
+  display.clearDisplay();
+  display.drawBitmap( 0, 0, cat3, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(300);
+
+  display.clearDisplay();
+  display.drawBitmap( 0, 0, cat4, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(1000);
+
+  for(int i=0;i<5;i++){
+    display.clearDisplay();
+    display.drawBitmap( 0, 0, cat5, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(200);
+  
+    display.clearDisplay();
+    display.drawBitmap( 0, 0, cat6, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(200);
+  
+    display.clearDisplay();
+    display.drawBitmap( 0, 0, cat7, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(200);
+  
+    display.clearDisplay();
+    display.drawBitmap( 0, 0, cat6, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(200);
+    display.clearDisplay();
+    display.drawBitmap( 0, 0, cat5, LOGO_WIDTH, LOGO_HEIGHT, 1);
+    display.display();
+    delay(200);
+  }
+  
+  display.clearDisplay();
+  display.display();
+
+/* ***************************************************************************** */
+/* ************************** Setup - FAN  ************************************* */
+/* ***************************************************************************** */
+  pinMode(FAN, OUTPUT);
+  digitalWrite(FAN, LOW);
+
+  
+}
+
+
+void loop() {
+/* ***************************************************************************** */
+/* ************************** Loop - Dust Sensor   ***************************** */
+/* ***************************************************************************** */
+  digitalWrite(V_LED, LOW);
+  delayMicroseconds(280);
+  Vo_value = analogRead(Vo);
+  delayMicroseconds(40);
+  digitalWrite(V_LED, HIGH);
+  delayMicroseconds(9680);
+
+  Voltage = Vo_value / 1024 * 5.0;
+  dustDensity = (Voltage - 0.3) / 0.005;
+  Serial.println(dustDensity);
+
+  
+  delay(1000);
+
+
+
+/* ***************************************************************************** */
+/* ************************** Loop - LCD   ************************************* */
+/* ***************************************************************************** */
+    display.clearDisplay();     //lcd 화면을 지웁니다.
+    display.setCursor(10,10);     //lcd 커서 위치를 10,10로 위치시킵니다.
+    display.println(F("KNU Air Cleaner"));
+    display.setCursor(10,30);     //lcd 커서 위치를 10,30로 위치시킵니다.
+    if( dustDensity > 150 ){    // 매우 나쁨 //
+      display.println("AIR : VERY BAD!!");
+    }else if( dustDensity > 80){   // 나쁨 //
+      display.println("AIR : BAD!      ");   
+    }else if( dustDensity > 30){  // 보통 //
+      display.println("AIR : NORMAL     ");   
+    }else{                    // 좋음
+      display.println("AIR : GOOD      ");   
+    }
+    display.setCursor(10,40);     //lcd 커서 위치를 10,40로 위치시킵니다.
+    display.print("ug/m3:");
+    display.println(dustDensity);
+    display.drawBitmap( 104, 46, cat_foot, 24, 18, 1);
+
+    display.display();
+
+
+/* ***************************************************************************** */
+/* ************************** Loop - FAN   ************************************* */
+/* ***************************************************************************** */
+  if(dustDensity > 80){
+    digitalWrite(FAN, HIGH);
+  }else{
+    digitalWrite(FAN, LOW);    
+  }
+
+  delay(1000);
+  
+}
